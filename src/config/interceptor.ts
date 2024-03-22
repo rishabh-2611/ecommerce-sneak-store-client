2f72e949b26dc281fc5ac87f2800553de1719212
rishabh-2611 <rishabh.sontakke@gmail.com>
2024-03-23 05:14:39 +0530

    Hanle 401 unauthorized response in axios interceptor

--- src/config/interceptor.ts
+++ src/config/interceptor.ts
@@ -1,6 +1,14 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 // eslint-disable-next-line no-undef
 const serverApp = `${process.env.SERVER_APP}/api/v1`;
 
 axios.defaults.baseURL = serverApp;
 axios.defaults.withCredentials = true;
+
+axios.interceptors.response.use((response) => response, (error) => {
+    if (error.response.status === 401) {
+        localStorage.clear();
+        window.location.href = '/';
+    }
+});
